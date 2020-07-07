e0987921892f10a34f8ccadbc04f8db1c527db46
Raghavender <raghavender.n@sixt.com>
2020-07-07 20:31:38 +0530

    updated the repo

--- src/main/java/ImageHoster/ImageHosterApplication.java
+++ src/main/java/ImageHoster/ImageHosterApplication.java
@@ -1,10 +1,13 @@
-package com.imagehoster.application;
+package ImageHoster;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
+@EnableAutoConfiguration
 public class ImageHosterApplication {
     public static void main(String[] args) {
         SpringApplication.run(ImageHosterApplication.class, args);
